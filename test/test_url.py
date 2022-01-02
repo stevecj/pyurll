c5598525c5f9190299311dbfcb1d32ab56ba6458
Steve Jorgensen <stevej@cliosoft.com>
2022-01-01 16:29:11 -0800

    Introduce netwk url & fragment part classes

--- test/test_url.py
+++ test/test_url.py
@@ -1,6 +1,6 @@
 import pytest
 
-from urll import URL
+from urll import FragmentPart, URL, NetworkURL
 
 
 FULL_URL_TEXT = (
@@ -16,10 +16,14 @@ class TestBuiltFromFullURL:
         return URL(FULL_URL_TEXT)
 
     def test_exposes_network_url(self, url_inst):
-        assert str(url_inst.network_url) == (
+        network_url = url_inst.network_url
+        assert isinstance(network_url, NetworkURL)
+        assert str(network_url) == (
             'http://www.example.com:80/path/to/resource'
             '?key1=value1&key2=value2'
         )
 
     def test_exposes_fragment_part(self, url_inst):
-        assert str(url_inst.fragment_part) == '#PlaceInDocument'
+        fragment_part = url_inst.fragment_part
+        assert isinstance(fragment_part, FragmentPart)
+        assert str(fragment_part) == '#PlaceInDocument'
--- urll/__init__.py
+++ urll/__init__.py
@@ -9,8 +9,26 @@ class URL:
 
     @property
     def network_url(self):
-        return f'{self._scheme}://{self._netloc}{self._path}?{self._query}'
+        return NetworkURL(
+            f'{self._scheme}://{self._netloc}{self._path}?{self._query}'
+        )
 
     @property
     def fragment_part(self):
-        return f'#{self._fragment}'
+        return FragmentPart(f'#{self._fragment}')
+
+
+class NetworkURL:
+    def __init__(self, network_url_text):
+        self._init_text = network_url_text
+
+    def __str__(self):
+        return self._init_text
+
+
+class FragmentPart:
+    def __init__(self, fragment_part_text):
+        self._init_text = fragment_part_text
+
+    def __str__(self):
+        return self._init_text
